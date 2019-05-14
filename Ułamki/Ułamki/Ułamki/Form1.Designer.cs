8f71c597efc89303b34f287778b72233d299163a
Julian <48802692+Lujen@users.noreply.github.com>
2019-05-14 21:02:02 +0200

    Add files via upload

---
+++ Ułamki/Ułamki/Ułamki/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Ułamki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(29, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 205);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(29, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(231, 179);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(29, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(231, 205);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(29, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(356, 179);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(29, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(356, 205);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(29, 20);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox7.Location = new System.Drawing.Point(141, 187);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(24, 32);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(293, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "=";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 34);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Dodawanie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(159, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 34);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Odejmowanie";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(273, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 34);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Mnożenie";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(107, 333);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 34);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Dzielenie";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(220, 333);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 34);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Czyszczenie";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(45, 191);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(27, 20);
+            this.textBox8.TabIndex = 13;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(198, 191);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(27, 20);
+            this.textBox9.TabIndex = 14;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(323, 191);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(27, 20);
+            this.textBox10.TabIndex = 15;
+            this.textBox10.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 450);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+    }
+}
+
---
+++ Ułamki/Ułamki/Ułamki/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ułamki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="" || textBox3.Text=="")
+            {
+                MessageBox.Show("Podaj licznik!");
+                return;
+            }
+            if (textBox8.Text == "")
+            {
+                textBox8.Text = "0";
+            }
+            if(textBox9.Text == "")
+            {
+                textBox9.Text = "0";
+            }
+            if(textBox2.Text=="0" || textBox2.Text=="" || textBox4.Text=="0" || textBox4.Text=="")
+            {
+                MessageBox.Show("Błędny mianownik!");
+                return;
+            }
+                int licznik = int.Parse(textBox1.Text);
+                int mianownik = int.Parse(textBox2.Text);
+                int licznik2 = int.Parse(textBox3.Text);
+                int mianownik2 = int.Parse(textBox4.Text);
+                int calosc = int.Parse(textBox8.Text);
+                int calosc2 = int.Parse(textBox9.Text);
+                int L_wynik = (((calosc * mianownik) + licznik) * mianownik2) + (mianownik * (licznik2 + (calosc2 * mianownik2)));
+                int M_wynik = (mianownik * mianownik2);
+                int C_wynik=0;
+            if (L_wynik >= M_wynik)
+            {
+                C_wynik = L_wynik / M_wynik;
+                L_wynik = L_wynik % M_wynik;
+
+                int a = L_wynik;
+                int b = M_wynik;
+                while (a != b)
+                {
+                    if (a > b)
+                        a -= b;
+                    else
+                        b -= a;
+                }
+                L_wynik = L_wynik / a;
+                M_wynik = M_wynik / a;
+            }
+
+            textBox5.Text = L_wynik.ToString();
+                textBox6.Text = M_wynik.ToString();
+                textBox10.Text = C_wynik.ToString();
+                textBox7.Text = "+";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("Podaj licznik!");
+                return;
+            }
+            if (textBox8.Text == "")
+            {
+                textBox8.Text = "0";
+            }
+            if (textBox9.Text == "")
+            {
+                textBox9.Text = "0";
+            }
+            if (textBox2.Text == "0" || textBox2.Text == "" || textBox4.Text == "0" || textBox4.Text == "")
+            {
+                MessageBox.Show("Błędny mianownik!");
+                return;
+            }
+            int licznik = int.Parse(textBox1.Text);
+            int mianownik = int.Parse(textBox2.Text);
+            int licznik2 = int.Parse(textBox3.Text);
+            int mianownik2 = int.Parse(textBox4.Text);
+            int calosc = int.Parse(textBox8.Text);
+            int calosc2 = int.Parse(textBox9.Text);
+            int L_wynik = (((calosc*mianownik) + licznik) * mianownik2) - (mianownik * (licznik2+(calosc2*mianownik2)));
+            int M_wynik = (mianownik * mianownik2);
+            int C_wynik = 0;
+            if (L_wynik >= M_wynik)
+            {
+                C_wynik = L_wynik / M_wynik;
+                L_wynik = L_wynik % M_wynik;
+
+                int a = L_wynik;
+                int b = M_wynik;
+                while (a != b)
+                {
+                    if (a > b)
+                        a -= b;
+                    else
+                        b -= a;
+                }
+                L_wynik = L_wynik / a;
+                M_wynik = M_wynik / a;
+            }
+                textBox5.Text = L_wynik.ToString();
+            textBox6.Text = M_wynik.ToString();
+            textBox10.Text = C_wynik.ToString();
+            textBox7.Text = "-";
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("Podaj licznik!");
+                return;
+            }
+            if (textBox8.Text == "")
+                {
+                    textBox8.Text = "0";
+                }
+            if (textBox9.Text == "")
+                {
+                    textBox9.Text = "0";
+                }
+            if (textBox2.Text == "0" || textBox2.Text == "" || textBox4.Text == "0" || textBox4.Text == "")
+                {
+                    MessageBox.Show("Błędny mianownik!");
+                    return;
+                }
+                int licznik = int.Parse(textBox1.Text);
+                int mianownik = int.Parse(textBox2.Text);
+                int licznik2 = int.Parse(textBox3.Text);
+                int mianownik2 = int.Parse(textBox4.Text);
+                int calosc = int.Parse(textBox8.Text);
+                int calosc2 = int.Parse(textBox9.Text);
+                int L_wynik = (((calosc*mianownik) + licznik) * (licznik2+(calosc2*mianownik2)));
+                int M_wynik = (mianownik * mianownik2);
+            int C_wynik = 0;
+            if (L_wynik >= M_wynik)
+            {
+                C_wynik = L_wynik / M_wynik;
+                L_wynik = L_wynik % M_wynik;
+
+                int a = L_wynik;
+                int b = M_wynik;
+                while (a != b)
+                {
+                    if (a > b)
+                        a -= b;
+                    else
+                        b -= a;
+                }
+                L_wynik = L_wynik / a;
+                M_wynik = M_wynik / a;
+            }
+                textBox5.Text = L_wynik.ToString();
+            textBox6.Text = M_wynik.ToString();
+            textBox10.Text = C_wynik.ToString();
+            textBox7.Text = "*";
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("Podaj licznik!");
+                return;
+            }
+            if (textBox8.Text == "")
+            {
+                textBox8.Text = "0";
+            }
+            if (textBox9.Text == "")
+            {
+                textBox9.Text = "0";
+            }
+            if (textBox2.Text == "0" || textBox2.Text == "" || textBox4.Text == "0" || textBox4.Text == "")
+            {
+                MessageBox.Show("Błędny mianownik!");
+                return;
+            }
+            int licznik = int.Parse(textBox1.Text);
+            int mianownik = int.Parse(textBox2.Text);
+            int licznik2 = int.Parse(textBox3.Text);
+            int mianownik2 = int.Parse(textBox4.Text);
+            int calosc = int.Parse(textBox8.Text);
+            int calosc2 = int.Parse(textBox9.Text);
+            int L_wynik = (((calosc * mianownik) + licznik) * mianownik2);
+            int M_wynik = (mianownik * (licznik2 + (calosc2 * mianownik2)));
+            int C_wynik = 0;
+            if (L_wynik >= M_wynik)
+            {
+                C_wynik = L_wynik / M_wynik;
+                L_wynik = L_wynik % M_wynik;
+
+                int a = L_wynik;
+                int b = M_wynik;
+                while (a != b)
+                {
+                    if (a > b)
+                        a -= b;
+                    else
+                        b -= a;
+                }
+                L_wynik = L_wynik / a;
+                M_wynik = M_wynik / a;
+            }
+                textBox5.Text = L_wynik.ToString();
+            textBox6.Text = M_wynik.ToString();
+            textBox10.Text = C_wynik.ToString();
+            textBox7.Text = "/";
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+            textBox9.Clear();
+            textBox10.Clear();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
